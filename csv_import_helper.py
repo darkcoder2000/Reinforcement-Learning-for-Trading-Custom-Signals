7c2a063577d4d4302ac09d0e7977652782d3d998
Martin Scholz <martin.scholz@gmx.net>
2022-01-07 10:35:13 +0100

    Enhanced scripts for more detailed and extensive processing

---
+++ csv_import_helper.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import os
+
+def import_csv_from_cryptodatadownload(csv_file):
+    df = pd.read_csv(csv_file)
+
+    #correct date format
+    df['date'] = pd.to_datetime(df['date'])
+
+    #drop columns and prepare volume column
+    df = df.drop(columns=['symbol','Volume USDT','tradecount'])
+    df.rename(columns={'Volume ETH': 'volume'}, inplace=True)
+
+    #sort entries by date to make sure oldest entries come first
+    df.sort_values('date', ascending=True, inplace=True)
+    df.set_index('date', inplace=True)
+    return df
+
+def import_example_sin_data(length):
+    in_array = np.linspace(0, 150, length)
+    out_array = (np.sin(in_array)*40) + 100
+
+    df = pd.DataFrame(data=out_array, columns=["price"])
+    # df['open'] = df['close']
+    # df['high'] = df['close']
+    # df['low'] = df['close']
+    # df['volume'] = 1000.0
+
+    return df
